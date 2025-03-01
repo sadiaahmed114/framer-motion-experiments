fa3037d375d3d00a3573dcc8b3bf348ae6a1f2d6
Sadia Ahmed <sadiaahmed26373@gmail.com>
2025-02-28 20:54:37 -0800

    Exit_Enter_Animation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import TextAnimation from "./components/TextAnimation/TextAnimaton";
+import Exit_Enter_Animation from "./components/Exit_Enter_Animation/Exit_Enter_Animation";
 function App() {
   return (
     <div className="App">
       {/* <h1>Framer Motion Experiments</h1> */}
       <TextAnimation />
+      <Exit_Enter_Animation/>
     </div>
   );
 }
---
+++ src/components/Exit_Enter_Animation/Exit_Enter_Animation.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useState } from 'react';
+import {motion , AnimatePresence} from 'framer-motion';
+
+function Exit_Enter_Animation() {
+    const[show , setShow] = useState(true)
+  return (
+    <div>
+        style ={{
+            display : "flex",
+            gap: "10px",
+            alignItem: "center",
+            flexDirection: "column"
+        }}
+
+        <h1 style={{marginTop :"30px"}}>        <h1 style={{marginTop :"30px"}}>Exit_Enter_Animation</h1>
+        </h1>
+        <AnimatePresence>
+            {show && <motion.div
+                style = {{width : "100px" , height: "100px" , backgroundColor : "orange"}}
+                initial= {{opacity: 0, x: 50}}
+                animate = {{opacity : 1, x : 100}}
+                exit = {{opacity : 0 , x : -150}}
+                transition = {{
+                    duration :"1"
+                }}
+            >
+
+            </motion.div>
+            }
+        </AnimatePresence>
+        <button onClick ={() => setShow(!show)} style={{padding : "12px 18px"}} > {show?"Remove Box" : "Add Box" }
+
+        </button>
+    </div>
+  )
+}
+
+export default Exit_Enter_Animation
+
